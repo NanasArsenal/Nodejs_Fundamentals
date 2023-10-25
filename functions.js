c67a6c37e84f428e6bbed906f8ba2f5910412633
NanasArsenal <nanakwekudev@gmail.com>
2023-10-25 05:11:15 +0100

    functions

---
+++ functions.js
@@ -0,0 +1,10 @@
+//functions are a block of code that perform a particular action 
+ function sumofNums(a,b){
+    let sum = a+b;
+    return sum;
+    
+ }
+
+ //So function can be used to perform lots of actions in javascipt. they can be used for computation and Api calls and even much more complex stuff
+
+console.log(sumofNums(9,10))
